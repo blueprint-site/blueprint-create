33016612c18a93515923a5b41355fc9027e3238a
HowToCodeDE <florian.reintgen@gmail.com>
2024-05-05 13:57:03 +0200

    Add react-i18next

--- blueprint/src/App.tsx
+++ blueprint/src/App.tsx
@@ -6,6 +6,26 @@ import Home from "./components/Home";
 import { Suspense } from "react";
 import Loading from "./components/Loading";
 
+
+import EnglishTranslation from './assets/en_us.json';
+
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+
+i18n
+    .use(initReactI18next) // passes i18n down to react-i18next
+    .init({
+        resources: {
+            en: EnglishTranslation
+        },
+        lng: "en", // if you're using a language detector, do not define the lng option
+        fallbackLng: "en",
+
+        interpolation: {
+            escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+        }
+    });
+
 const App = () => {
     return (
         <BrowserRouter>
--- blueprint/src/components/Navigaton.tsx
+++ blueprint/src/components/Navigaton.tsx
@@ -1,32 +1,25 @@
-import i18next from 'i18next';
-
-import EnglishTranslation from '../assets/en_us.json';
+import { useTranslation } from "react-i18next";
 
 import BlueprintLogo from '../assets/logo.webp';
 import AddonIcon from '../assets/minecart_coupling.webp';
 import SchematicIcon from '../assets/schematic.webp';
 import AboutIcon from '../assets/clipboard.webp'
 import NavigationLink from './NavigationLink';
-import '../styles/navigation.scss';
 import { NavLink } from 'react-router-dom';
 
+import '../styles/navigation.scss';
+
 const Navigation = () => {
-    i18next.init({
-        lng: 'en', // if you're using a language detector, do not define the lng option
-        debug: true,
-        resources: {
-            en: EnglishTranslation
-        }
-    });
+    const { t } = useTranslation();
 
     return (
         <>
             <nav>
                 <NavLink className="logo" to="/"><img src={BlueprintLogo} alt="Logo" />Blueprint</NavLink>
                 <span />
-                <NavigationLink destination={'/addons'} icon={AddonIcon} label={i18next.t("navigation.label.addons")}></NavigationLink>
-                <NavigationLink destination={'/schematics'} icon={SchematicIcon} label={i18next.t("navigation.label.schematics")}></NavigationLink>
-                <NavigationLink destination={'/about'} icon={AboutIcon} label={i18next.t("navigation.label.about")}></NavigationLink>
+                <NavigationLink destination={'/addons'} icon={AddonIcon} label={t("navigation.label.addons")}></NavigationLink>
+                <NavigationLink destination={'/schematics'} icon={SchematicIcon} label={t("navigation.label.schematics")}></NavigationLink>
+                <NavigationLink destination={'/about'} icon={AboutIcon} label={t("navigation.label.about")}></NavigationLink>
             </nav>
         </>
     );
