15231c69d75a18dc5174d6cd3b96b23cfd18f71a
HowToCodeDE <florian.reintgen@gmail.com>
2024-05-06 16:41:05 +0200

    Change style of Alert

--- src/components/ErrorAlert.tsx
+++ src/components/ErrorAlert.tsx
@@ -3,15 +3,21 @@ interface ErrorAlertProperties {
     body: string,
 }
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Alert from 'react-bootstrap/Alert';
+
+import '../styles/alert.scss';
 
 function ErrorAlert({ heading, body }: ErrorAlertProperties) {
     const [show, setShow] = useState(true);
 
+    useEffect(() => {
+        window.setTimeout(() => { setShow(false) }, 5000);
+    }, []);
+
     if (show) {
         return (
-            <Alert variant="danger absolute w-30" onClose={() => setShow(false)} dismissible>
+            <Alert variant="danger" className='alert-absolute' onClose={() => setShow(false)} dismissible>
                 <Alert.Heading>{heading}</Alert.Heading>
                 <p>
                     {body}
