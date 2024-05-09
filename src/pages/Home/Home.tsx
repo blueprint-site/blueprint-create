117a837a0693c1f4acf696e142128d8f134a763d
Yehor <omelianenkoegor@gmail.com>
2024-05-09 18:33:22 +0200

    Implemented working-ish image slideshow-ish

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import AddonsSlideshow from "../../components/AddonsSlideshow";
 import AwardsBanner from "../../components/AwardsBanner";
 import DiscoverAddonsText from "../../components/DiscoverAddonsText";
+import HomeAddonList from "../../components/HomeAddonList";
 function Home() {
   return (
     <>
       <AwardsBanner />
       <DiscoverAddonsText />
       <AddonsSlideshow />
+      <HomeAddonList />
     </>
   );
 }
