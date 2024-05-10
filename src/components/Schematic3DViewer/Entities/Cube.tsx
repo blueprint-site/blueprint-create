54da18783e5f555fe7e45b961fb0a739aaea607c
Matías Fuentes <matiasf.cortez@gmail.com>
2024-05-10 08:39:12 -0400

    fix: lag issues with when uploading nbt files

--- src/components/Schematic3DViewer/Entities/Cube.tsx
+++ src/components/Schematic3DViewer/Entities/Cube.tsx
@@ -1,40 +1,34 @@
-import { useBox } from "@react-three/cannon";
-import { useState } from "react";
-import { Mesh } from "three";
+import { BoxGeometry, InstancedMesh, Object3D } from "three";
 import { textures } from "../Textures/textureLoader";
+import { useEffect, useRef } from "react";
+import { useSchematicStore } from "../store/schematic";
 
-type CubeProps = {
-  position: [number, number, number];
-  texture: string;
-};
+// type CubeProps = {
+//   position: [number, number, number];
+// };
 
-export function Cube({ position, texture }: CubeProps) {
-  const [isHovered, setIsHovered] = useState(false);
-  const [ref] = useBox<Mesh>(() => ({
-    type: "Static",
-    position,
-  }));
-  const activeTexture = textures.get(texture);
+const box = new BoxGeometry();
+const cube = new Object3D();
+export function Cube() {
+  const ref = useRef<InstancedMesh>(null);
+  const { schematic } = useSchematicStore();
+
+  const activeTexture = textures.get("plank");
+
+  useEffect(() => {
+    // Set positions
+    schematic.forEach(({ pos }, index) => {
+      cube.position.set(pos[0], pos[1], pos[2]);
+      cube.updateMatrix();
+      ref.current?.setMatrixAt(index, cube.matrix);
+    });
+    // Update the instance
+    if (ref.current) ref.current.instanceMatrix.needsUpdate = true;
+  }, [schematic, ref]);
 
   return (
-    <mesh
-      ref={ref}
-      onPointerMove={(e) => {
-        e.stopPropagation();
-        setIsHovered(true);
-      }}
-      onPointerOut={(e) => {
-        e.stopPropagation();
-        setIsHovered(false);
-      }}
-    >
-      <boxGeometry attach="geometry" />
-      <meshStandardMaterial
-        color={isHovered ? "grey" : "white"}
-        map={activeTexture}
-        transparent={true}
-        attach="material"
-      />
-    </mesh>
+    <instancedMesh ref={ref} args={[box, undefined, schematic.length]}>
+      <meshBasicMaterial attach="material" map={activeTexture} />
+    </instancedMesh>
   );
 }
