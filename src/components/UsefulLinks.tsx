5c1a57d3c53095577fede0902e81af47928a1104
Yehor <omelianenkoegor@gmail.com>
2024-05-10 20:19:45 +0200

    Added "Open in a new tab" to the links

--- src/components/UsefulLinks.tsx
+++ src/components/UsefulLinks.tsx
@@ -8,9 +8,9 @@ function UsefulLinks() {
             <h1>Here are some useful Create Mod links:</h1>
             <a href="https://create.fandom.com/wiki/Create_Mod_Wiki" target="_blank"><img src={WikiLogo} alt=""/>Create Mod wiki</a>
             <br />
-            <a href="https://modrinth.com/mod/create"><img src={CreateLogo} alt="" />Create Mod (Forge)</a>
+            <a href="https://modrinth.com/mod/create" target="_blank"><img src={CreateLogo} alt="" />Create Mod (Forge)</a>
             <br />
-            <a href="https://modrinth.com/mod/create-fabric"><img src={CreateLogo} alt="createmodlogo" />Create Mod (Fabric)</a>
+            <a href="https://modrinth.com/mod/create-fabric" target="_blank"><img src={CreateLogo} alt="createmodlogo" />Create Mod (Fabric)</a>
             </center>
         </div>
     );
