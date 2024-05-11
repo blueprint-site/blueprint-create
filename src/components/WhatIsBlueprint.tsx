44aad9d6fb37fe9a0338265259d89f76275130d9
HowToCodeDE <florian.reintgen@gmail.com>
2024-05-11 14:07:04 +0200

    Some fixes

--- src/components/WhatIsBlueprint.tsx
+++ src/components/WhatIsBlueprint.tsx
@@ -3,13 +3,16 @@ import SchematicIcon from "../assets/schematic.webp"
 import BlueprintLogo from "../assets/logo.webp"
 
 import "../styles/whatisblueprint.scss"
+import { useTranslation } from "react-i18next"
 
 function WhatIsBlueprint() {
+    const { t } = useTranslation();
+
     return (
         <div className="what-is-blueprint">
             <center>
-                <h1>What is Blueprint?</h1>
-                <h4>Blueprint is a Create Mod themed site!</h4>
+                <h1>{t("home.info.about.title")}</h1>
+                <h4>{t("home.info.about.description")}</h4>
                 <img src={AddonIcon} alt="" />+<img src={SchematicIcon} alt="" />= <img src={BlueprintLogo} alt="" />
             </center>
         </div>
