abbde75184c93fa14f0a69dd5d1eec0229ad7497
HowToCodeDE <florian.reintgen@gmail.com>
2024-05-12 17:32:46 +0200

    Add back slideshow

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import { Suspense, lazy } from "react";
 
 import LoadingOverlay from "../../components/LoadingOverlay";
 
+const AddonsSlideshow = lazy(() => import("../../components/AddonsSlideshow"));
 const AwardsBanner = lazy(() => import("../../components/AwardsBanner"));
 const DiscoverAddonsText = lazy(() => import("../../components/DiscoverAddonsText"));
 const WhatIsBlueprint = lazy(() => import("../../components/WhatIsBlueprint"));
@@ -14,6 +15,7 @@ function Home() {
       <Suspense fallback={<LoadingOverlay />}>
         <AwardsBanner />
         <DiscoverAddonsText />
+        <AddonsSlideshow />
         <WhatIsBlueprint />
         <UsefulLinks />
         <Contributors />
