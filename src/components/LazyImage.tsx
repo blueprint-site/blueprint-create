9f4df9122adbab15d670e4124a0babb516a5d587
HowToCodeDE <florian.reintgen@gmail.com>
2024-05-12 22:34:27 +0200

    Make slideshow lazy loading to even further improve page speed

--- src/components/LazyImage.tsx
+++ src/components/LazyImage.tsx
@@ -3,13 +3,14 @@ import LazyLoad from 'react-lazyload';
 interface LazyImageProperties {
     src: string,
     alt: string,
+    className?: string,
     height?: number | string,
     width?: number | string,
 }
 
-const LazyImage = ({ src, alt, height }: LazyImageProperties) => {
+const LazyImage = ({ src, alt, className, height }: LazyImageProperties) => {
     return (
-        <LazyLoad height={height} offset={150}>
+        <LazyLoad className={className} height={height} offset={150}>
             {/* The height and offset props control when the image should start loading */}
             <img src={src} alt={alt} height={height} />
         </LazyLoad>
