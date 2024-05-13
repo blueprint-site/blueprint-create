7260b023e869e858e0039b4bdff9fe4b6339f6d4
Matías Fuentes <matiasf.cortez@gmail.com>
2024-05-13 11:19:55 -0400

    perf: better performance on schematics page

--- src/components/Schematic3DViewer/Entities/Cube.tsx
+++ src/components/Schematic3DViewer/Entities/Cube.tsx
@@ -3,26 +3,19 @@ import { textures } from "../Textures/textureLoader";
 import { useEffect, useRef } from "react";
 import { useSchematicStore } from "../store/schematic";
 
-// type CubeProps = {
-//   position: [number, number, number];
-// };
-
 const box = new BoxGeometry();
 const cube = new Object3D();
+const activeTexture = textures.get("plank");
 export function Cube() {
   const ref = useRef<InstancedMesh>(null);
   const { schematic } = useSchematicStore();
 
-  const activeTexture = textures.get("plank");
-
   useEffect(() => {
-    // Set positions
     schematic.forEach(({ pos }, index) => {
       cube.position.set(pos[0], pos[1], pos[2]);
       cube.updateMatrix();
       ref.current?.setMatrixAt(index, cube.matrix);
     });
-    // Update the instance
     if (ref.current) ref.current.instanceMatrix.needsUpdate = true;
   }, [schematic, ref]);
 
