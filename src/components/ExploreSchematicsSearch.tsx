799e31d0e162a4325ae381a62e783d49be4f3fbc
Yehor <omelianenkoegor@gmail.com>
2024-05-18 12:30:28 +0200

    Added very early version of Explore Schematics page

--- src/components/ExploreSchematicsSearch.tsx
+++ src/components/ExploreSchematicsSearch.tsx
@@ -11,13 +11,26 @@ function ExploreSchematicsSearch() {
                             Type
                         </Dropdown.Toggle>
                         <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">Train</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">Farm</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">Factory</Dropdown.Item>
-                            <Dropdown.Item href="#/action-4">Building</Dropdown.Item>
+                            <Dropdown.Item href="#/category-train">Train</Dropdown.Item>
+                            <Dropdown.Item href="#/category-farm">Farm</Dropdown.Item>
+                            <Dropdown.Item href="#/category-factory">Factory</Dropdown.Item>
+                            <Dropdown.Item href="#/category-building">Building</Dropdown.Item>
                         </Dropdown.Menu>
                     </Dropdown>
-                </div>
+            </div>
+            <div className="dropdown-container">
+                    <Dropdown>
+                        <Dropdown.Toggle>
+                            Game Version
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="#/for-1.20.1">1.20.1</Dropdown.Item>
+                            <Dropdown.Item href="#/for-1.19.2">1.19.2</Dropdown.Item>
+                            <Dropdown.Item href="#/for-1.18.2">1.18.2</Dropdown.Item>
+                            <Dropdown.Item href="#/for-1.16.5">1.16.5</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+            </div>
         </div>
     );
 }
--- src/components/ExploreSchematicsTitle.tsx
+++ src/components/ExploreSchematicsTitle.tsx
@@ -1,12 +1,13 @@
 import { useTranslation } from "react-i18next";
 import '../styles/exploreschematicstitle.scss';
+import Experience_Block from "../assets/block_of_experience.webp"
 
 function ExploreSchematicsTitle() {
     return (
-        <>
+        <div className="exploreschematicstitle-container">
         <h1>Explore Schematics</h1>
-        <a href="/schematics/3dviewer">Wanna see your schematic in 3D?</a>
-        </>
+        <a href="/schematics/3dviewer" className="a">Wanna see your schematic in 3D? <img src={Experience_Block} alt="" /></a>
+        </div>
     )
 };
 
