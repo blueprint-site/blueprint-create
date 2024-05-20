5bdef37c614d4c7958d655544a82d333a28d56b9
Yehor <omelianenkoegor@gmail.com>
2024-05-20 16:02:36 +0200

    Added Chematic cards to /schematics page

--- src/components/ExploreSchematicsSchematics.tsx
+++ src/components/ExploreSchematicsSchematics.tsx
@@ -1,18 +1,64 @@
 import "../styles/exploreschematicsschematics.scss"
 import WorldDestroyer from "../assets/schematic-images/world_destroyer.webp"
+import IndustrialBuildings from "../assets/schematic-images/industrial_buildings.webp"
+import InsideFactory from "../assets/schematic-images/inside_factory.webp"
+import BulkProcessing from "../assets/schematic-images/bulk_processing.webp"
+import Card from 'react-bootstrap/Card';
 
 function ExploreSchematicsSchematics() {
+    const versions = ["1.16.5", "1.18.2", "1.19.2", "1.20.1"]
     return (
-        <div className="schematics">
-            <div className="schematic">
-                <h1>Title</h1>
-                <img src={WorldDestroyer} alt="" />
-                <h3>Description</h3>
-                <div>
-                    <button className="download-button">Download</button><button className="ThreeD-button">View in 3D</button>
-                </div>
-            </div>
-        </div>
+    <div className="schematics">
+        <Card className="schematic">
+            <Card.Img variant="top" src={WorldDestroyer} />
+        <Card.Body>
+            <Card.Title>Schematic name</Card.Title>
+        <Card.Text>
+            For Minecraft {versions[0]}<br/>
+          Schematic Description
+        </Card.Text>
+            <a className="download-button">Download</a>
+            <a className="ThreeD-button">3D View</a>
+        </Card.Body>
+        </Card>
+        <Card className="schematic">
+            <Card.Img variant="top" src={IndustrialBuildings} />
+        <Card.Body>
+            <Card.Title>Schematic name</Card.Title>
+        <Card.Text>
+            For Minecraft {versions[1]}<br/>
+          Schematic Description
+        </Card.Text>
+            <a className="download-button">Download</a>
+            <a className="ThreeD-button">3D View</a>
+        </Card.Body>
+        </Card>
+        <Card className="schematic">
+            <Card.Img variant="top" src={InsideFactory} />
+        <Card.Body>
+            <Card.Title>Schematic name</Card.Title>
+        <Card.Text>
+        For Minecraft {versions[2]}<br/>
+          Schematic Description
+        </Card.Text>
+            <a className="download-button">Download</a>
+            <a className="ThreeD-button">3D View</a>
+        </Card.Body>
+        </Card>
+        <Card className="schematic">
+            <Card.Img variant="top" src={BulkProcessing} />
+        <Card.Body>
+            <Card.Title>Schematic name</Card.Title>
+        <Card.Text>
+        For Minecraft {versions[3]}<br/>
+          Schematic Description
+        </Card.Text>
+            <a className="download-button">Download</a>
+            <a className="ThreeD-button">3D View</a>
+        </Card.Body>
+        </Card>
+        
+    </div>
     )
 }
 
