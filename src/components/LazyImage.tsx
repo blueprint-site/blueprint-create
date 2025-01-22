c62349cbcabdba116b7da240b798f843bc55ea54
spencerfrost <s.s.frost@gmail.com>
2025-01-22 13:51:05 -0500

    Enhance LazyImage component: add imgClassName prop and improve image styling for better responsiveness

--- src/components/LazyImage.tsx
+++ src/components/LazyImage.tsx
@@ -4,15 +4,38 @@ interface LazyImageProperties {
   src: string;
   alt: string;
   className?: string;
+  imgClassName?: string;
   height?: number | string;
   width?: number | string;
 }
 
-const LazyImage = ({ src, alt, className, height }: LazyImageProperties) => {
+const LazyImage = ({ 
+  src, 
+  alt, 
+  className,
+  imgClassName,
+  height, 
+  width 
+}: LazyImageProperties) => {
   return (
-    <LazyLoad className={className} height={height} offset={150}>
-      {/* The height and offset props control when the image should start loading */}
-      <img src={src} alt={alt} height={height} />
+    <LazyLoad 
+      className={className} 
+      height={height} 
+      offset={150}
+    >
+      <img 
+        src={src} 
+        alt={alt} 
+        className={imgClassName}
+        height={height}
+        width={width}
+        style={{ 
+          maxHeight: '100%',
+          maxWidth: '100%',
+          objectFit: 'contain',
+          objectPosition: 'center'
+        }}
+      />
     </LazyLoad>
   );
 };
