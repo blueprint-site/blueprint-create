5fc5af29c3ff2543c91e3361f77442591a78440f
Yehor <111529536+yehorscode@users.noreply.github.com>
2025-03-05 17:45:31 +0100

    Added cookie dialog animation (#237)
    * Added a cookie dialog animation on popup and hide
    
    * Added animation
    
    * Changed back to xl
    
    * Merge?

--- src/components/utility/CookieDialog.tsx
+++ src/components/utility/CookieDialog.tsx
@@ -1,7 +1,8 @@
 import { Button } from '@/components/ui/button';
 import { useEffect, useState } from 'react';
 import { cn } from '@/config/utils';
-import Cookie from '@/assets/cookie.webp'
+import { motion } from 'framer-motion';
+import Cookie from '@/assets/cookie.webp';
 
 export default function CookieDialog({
   variant = 'default',
@@ -15,17 +16,13 @@ export default function CookieDialog({
   const accept = () => {
     setIsOpen(false);
     document.cookie = 'cookieConsent=true; expires=Fri, 31 Dec 9999 23:59:59 GMT';
-    setTimeout(() => {
-      setHide(true);
-    }, 700);
+    setTimeout(() => setHide(true), 500);
     onAcceptCallback();
   };
 
   const decline = () => {
     setIsOpen(false);
-    setTimeout(() => {
-      setHide(true);
-    }, 700);
+    setTimeout(() => setHide(true), 500);
     onDeclineCallback();
   };
 
@@ -35,39 +32,36 @@ export default function CookieDialog({
       if (document.cookie.includes('cookieConsent=true')) {
         if (!demo) {
           setIsOpen(false);
-          setTimeout(() => {
-            setHide(true);
-          }, 700);
+          setTimeout(() => setHide(true), 500);
         }
       }
-    } catch (e) {
-      // console.log("Error: ", e);
-    }
+    } catch (e) {}
   }, []);
 
-  return variant != 'small' ? (
-    <div
+  return variant !== 'small' ? (
+    <motion.div
+      initial={{ y: 20, opacity: 0 }}
+      animate={isOpen ? { y: 0, opacity: 1 } : { y: 20, opacity: 0 }}
+      transition={{ type: 'spring', stiffness: 150, damping: 15 }}
       className={cn(
-        'fixed right-0 bottom-0 left-0 z-[200] w-full duration-700 sm:bottom-4 sm:left-4 sm:max-w-md',
-        !isOpen
-          ? 'translate-y-8 opacity-0 transition-[opacity,transform]'
-          : 'translate-y-0 opacity-100 transition-[opacity,transform]',
+        'fixed right-0 bottom-0 left-0 z-[200] w-full sm:bottom-4 sm:left-4 sm:max-w-md',
         hide && 'hidden'
       )}>
       <div className='dark:bg-card bg-background border-border m-3 rounded-md border shadow-lg'>
         <div className='grid gap-2'>
           <div className='border-border flex h-14 items-center justify-between border-b p-4'>
             <h1 className='my-0 p-0 text-lg leading-none font-medium'>We use cookies!</h1>
-            <img src={Cookie} alt="A Minecraft Cookie" className='h-10 w-10'/>
+            <img src={Cookie} alt='A Minecraft Cookie' className='h-10 w-10' />
           </div>
           <div className='p-4'>
             <p className='text-start text-sm font-normal'>
-              We use cookies to save some data and improve your experience. And because they are tasty of course. My favourite ones are the chockolate chip cookies.
+              We use cookies to save some data and improve your experience. And because they are
+              tasty of course. My favorite ones are chocolate chip cookies.
               <br />
               <br />
               <span className='text-xs'>
-                By clicking "<span className='font-medium opacity-80'>Accept</span>
-                ", you agree to our use of cookies.
+                By clicking "<span className='font-medium opacity-80'>Accept</span>", you agree to
+                our use of cookies.
               </span>
               <br />
               <a href='/privacy' className='text-xs underline'>
@@ -85,20 +79,20 @@ export default function CookieDialog({
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   ) : (
-    <div
+    <motion.div
+      initial={{ y: 20, opacity: 0 }}
+      animate={isOpen ? { y: 0, opacity: 1 } : { y: 20, opacity: 0 }}
+      transition={{ type: 'spring', stiffness: 150, damping: 15 }}
       className={cn(
-        'fixed right-0 bottom-0 left-0 z-[200] w-full duration-700 sm:bottom-4 sm:left-4 sm:max-w-md',
-        !isOpen
-          ? 'translate-y-8 opacity-0 transition-[opacity,transform]'
-          : 'translate-y-0 opacity-100 transition-[opacity,transform]',
+        'fixed right-0 bottom-0 left-0 z-[200] w-full sm:bottom-4 sm:left-4 sm:max-w-md',
         hide && 'hidden'
       )}>
       <div className='dark:bg-card bg-background border-border m-3 rounded-lg border'>
         <div className='flex items-center justify-between p-3'>
           <h1 className='text-lg font-medium'>We use cookies</h1>
-          <img src={Cookie} alt="Minecraft cookie" />
+          <img src={Cookie} alt='Minecraft cookie' />
         </div>
         <div className='-mt-2 p-3'>
           <p className='text-muted-foreground text-left text-sm'>
@@ -108,13 +102,13 @@ export default function CookieDialog({
         </div>
         <div className='mt-2 flex items-center gap-2 border-t p-3'>
           <Button onClick={accept} className='h-9 w-full rounded-full'>
-            accept
+            Accept
           </Button>
           <Button onClick={decline} className='h-9 w-full rounded-full' variant='outline'>
-            decline
+            Decline
           </Button>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
