67470c3737b4a91f700306bfcfec469d9efe973f
spencerfrost <s.s.frost@gmail.com>
2025-03-27 14:41:54 -0400

    Add addon verification UI components and workflow

---
+++ src/components/features/settings/addon-verification/index.tsx
@@ -0,0 +1,3 @@
+// Main exporter file for addon verification components
+export { default as AddonVerificationDialog } from './AddonVerificationDialog';
+export { default as AddonVerification } from './AddonVerification';
