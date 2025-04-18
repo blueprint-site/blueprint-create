e91d7559367f131e1e4654b267a2a915a650c5fc
Yehor <omelianenkoegor@gmail.com>
2025-04-18 19:32:46 +0200

    Added clicking interactions to the slideshow elements

--- src/components/features/home/AddonsSlideshow.tsx
+++ src/components/features/home/AddonsSlideshow.tsx
@@ -1,6 +1,5 @@
 import Autoplay from 'embla-carousel-autoplay';
 import { useEffect, useState } from 'react';
-
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import {
@@ -11,22 +10,21 @@ import {
 } from '@/components/ui/carousel';
 import MinecraftIcon from '../../utility/MinecraftIcon';
 import { useFetchFeaturedAddons } from '@/api/endpoints/useFeaturedAddons';
+import { Link } from 'react-router';
 
 const AddonsCarousel = () => {
   const [api, setApi] = useState<CarouselApi>();
   const [current, setCurrent] = useState(0);
   const { data: addons, isLoading, error } = useFetchFeaturedAddons();
+
   useEffect(() => {
     if (!api) return;
 
-    api.on('select', () => {
-      setCurrent(api.selectedScrollSnap());
-    });
+    const updateCurrent = () => setCurrent(api.selectedScrollSnap());
 
+    api.on('select', updateCurrent);
     return () => {
-      api.off('select', () => {
-        setCurrent(api.selectedScrollSnap());
-      });
+      api.off('select', updateCurrent);
     };
   }, [api]);
 
@@ -38,6 +36,9 @@ const AddonsCarousel = () => {
   if (error) return <p>Error loading addons!</p>;
   if (!addons || addons.length === 0) return <p>No featured addons available.</p>;
 
+  const currentAddon = addons[current];
+  const currentSlug = currentAddon?.slug ?? '#';
+
   return (
     <div className='mx-auto flex h-full max-w-6xl items-center justify-center gap-4'>
       <Button
@@ -48,7 +49,7 @@ const AddonsCarousel = () => {
         <MinecraftIcon name='chevron-left' size={32} />
       </Button>
 
-      <div className='relative flex-1'>
+      <Link to={`/addons/${currentSlug}`} className='group relative flex-1'>
         <Carousel
           setApi={setApi}
           opts={{ align: 'end', loop: true }}
@@ -61,34 +62,32 @@ const AddonsCarousel = () => {
                   loading='lazy'
                   src={addon.banner_url}
                   alt=''
-                  className='h-96 max-h-full rounded-lg'
+                  className='h-96 max-h-full w-full rounded-lg object-cover transition group-hover:brightness-80'
                 />
               </CarouselItem>
             ))}
           </CarouselContent>
         </Carousel>
-      </div>
+      </Link>
 
-      <div className='h-96 w-80'>
-        <Card className='bg-background h-full'>
+      <Link to={`/addons/${currentSlug}`} className='group h-96 w-80'>
+        <Card className='bg-background group-hover:bg-accent h-full cursor-pointer transition'>
           <CardHeader>
-            <CardTitle className='text-center text-2xl underline'>
-              {addons[current]?.title}
-            </CardTitle>
+            <CardTitle className='text-center text-2xl underline'>{currentAddon?.title}</CardTitle>
           </CardHeader>
           <CardContent className='flex flex-col items-center justify-center p-4'>
             <img
               loading='lazy'
-              className='h-full w-auto object-contain'
-              src={addons[current]?.image_url}
+              className='h-auto max-h-30 w-full object-contain'
+              src={currentAddon?.image_url}
               alt=''
             />
             <div className='mt-4 overflow-hidden'>
-              <p className='line-clamp-4 text-sm'>{addons[current]?.description}</p>
+              <p className='line-clamp-4 text-sm'>{currentAddon?.description}</p>
             </div>
           </CardContent>
         </Card>
-      </div>
+      </Link>
 
       <Button
         onClick={() => scrollToIndex(current + 1)}
