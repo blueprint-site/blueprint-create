fdfd52b1537a3698cce7a26624f0475bb66c2161
Yehor <111529536+yehorscode@users.noreply.github.com>
2025-05-24 12:37:35 +0200

    Update AddonCard.tsx

--- src/components/features/addons/addon-card/AddonCard.tsx
+++ src/components/features/addons/addon-card/AddonCard.tsx
@@ -1,4 +1,4 @@
-import { memo, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 
 import CategoryBadges from './CategoryBadges';
