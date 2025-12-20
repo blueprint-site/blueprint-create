a989258ffa874c30e5c26fca3aa828ac4804977c
Yehor <omelianenkoegor@gmail.com>
2025-12-20 11:02:33 +0100

    Add basic expanded addons framework

--- src/pages/Addons/ExpandedAddon/ExpandedAddonPage.tsx
+++ src/pages/Addons/ExpandedAddon/ExpandedAddonPage.tsx
@@ -1,14 +1,23 @@
-import { useParams } from "react-router-dom"
-import { useFetchAddonBySlug } from "@/utils/useAddons"
+import { useParams } from 'react-router-dom';
+import { useFetchAddonBySlug } from '@/utils/useAddons';
 export default function ExpandedAddonPage() {
-    const slug = useParams().slug?.toString()
-    const addon = useFetchAddonBySlug(slug)?.data
-    return (
-        <div className="">
-            {/* header */}
-            <div className="">
-               {addon && <img src={addon.icon} alt="" />} 
+  const slug = useParams().slug?.toString();
+  const addon = useFetchAddonBySlug(slug)?.data;
+  return (
+    <div className='py-2 px-10'>
+      <div className=''>
+        {addon && (
+          <div className=''>
+            <div className='flex items-center gap-4'>
+              <img src={addon.icon} alt={'Icon for ' + addon.name} className='w-20' />
+              <div className='flex flex-col'>
+                <span className='font-minecraft text-4xl'>{addon.name}</span>
+                <span>{addon.description}</span>
+              </div>
             </div>
-        </div>
-    )
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
