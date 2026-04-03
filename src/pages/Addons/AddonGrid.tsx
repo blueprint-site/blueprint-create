b3a5e82c73ed6e909965f80648ca9c6b6925c15b
Yehor <omelianenkoyehor@proton.me>
2026-04-03 18:24:16 +0200

    add thinking guy

--- src/pages/Addons/AddonGrid.tsx
+++ src/pages/Addons/AddonGrid.tsx
@@ -2,6 +2,7 @@ import type { Addon } from '@/types/addons';
 import type { z } from 'zod';
 import AddonCard from './AddonCard';
 import { Skeleton } from '@/components/ui/skeleton';
+import thinkimg from '@/assets/think.webp';
 // import { Input } from '@/components/ui/input';
 
 type AddonType = z.infer<typeof Addon>;
@@ -22,10 +23,7 @@ export default function AddonGrid({ data, isLoading = false }: AddonGridProps) {
         aria-live='polite'
       >
         {Array.from({ length: 12 }).map((_, index) => (
-          <div
-            key={`addon-skeleton-${index}`}
-            className='bg-surface-1 border text-white p-4'
-          >
+          <div key={`addon-skeleton-${index}`} className='bg-surface-1 border text-white p-4'>
             <Skeleton className='w-20 h-20 mb-3 rounded-full bg-surface-2' />
             <Skeleton className='h-5 w-2/3 mb-2 bg-surface-2' />
             <Skeleton className='h-4 w-full mb-2 bg-surface-2' />
@@ -40,7 +38,17 @@ export default function AddonGrid({ data, isLoading = false }: AddonGridProps) {
       </div>
     );
   }
-
+  if (data.length === 0) {
+    return (
+      <div className='text-center bg-surface-1 my-5 py-5 justify-center items-center flex flex-col'>
+        <img src={thinkimg} alt='Thinking' className='w-xs' />
+        <span className='font-minecraft text-3xl'>Well, this is weird</span>
+        <span className='text-sm opacity-80'>
+          No addons were found with your search criteria. Maybe adjust filters/change search ?
+        </span>
+      </div>
+    );
+  }
   return (
     <div className=''>
       <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-5'>
