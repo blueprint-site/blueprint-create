aee0bea5ea915b957a63502bb74d5836a49ec90a
Yehor <omelianenkoyehor@proton.me>
2026-04-03 18:45:00 +0200

    fix

--- src/pages/Addons/ExpandedAddon/ExpandedAddonCompatibilityAndVersions.tsx
+++ src/pages/Addons/ExpandedAddon/ExpandedAddonCompatibilityAndVersions.tsx
@@ -1,48 +1,56 @@
-import { Badge } from "@/components/ui/badge";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { fetchModrinthModDependencies } from "@/utils/getExternalAddon";
+import { Badge } from '@/components/ui/badge';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+// import { fetchModrinthModDependencies } from "@/utils/getExternalAddon";
 interface ExpandedAddonCompatibilityAndVersionsProps {
   versions: string[];
   authors?: string[];
 }
 
-export const ExpandedAddonCompatibilityAndVersions = ({ versions = [], authors = [] }: ExpandedAddonCompatibilityAndVersionsProps) => {
+export const ExpandedAddonCompatibilityAndVersions = ({
+  versions = [],
+  authors = [],
+}: ExpandedAddonCompatibilityAndVersionsProps) => {
+  // const dependency_data = fetchModrinthModDependencies('create')
+  // console.log(dependency_data)
 
-    const dependency_data = fetchModrinthModDependencies('create')
-    console.log(dependency_data)
-
-    return (
-        <div className="w-80 space-y-4 text-white">
-            <Card>
-                <CardHeader>
-                    <CardTitle className="font-minecraft text-xl">Versions</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <div className="flex flex-wrap gap-2">
-                        {versions.map(version => <Badge variant="secondary" key={version}>{version}</Badge>)}
-                    </div>
-                </CardContent>
-            </Card>
-            <Card>
-                <CardHeader>
-                    <CardTitle className="font-minecraft text-xl">Compatibility</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <p className="text-sm">No compatibility information available.</p>
-                </CardContent>
-            </Card>
-            <Card>
-                <CardHeader>
-                    <CardTitle className="font-minecraft text-xl">Authors</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <div className="flex flex-wrap gap-2">
-                        {authors.map(author => (
-                            <Badge variant="secondary" key={author}>{author}</Badge>
-                        ))}
-                    </div>
-                </CardContent>
-            </Card>
-        </div>
-    )
-}
+  return (
+    <div className='w-80 space-y-4 text-white'>
+      <Card>
+        <CardHeader>
+          <CardTitle className='font-minecraft text-xl'>Versions</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className='flex flex-wrap gap-2'>
+            {versions.map((version) => (
+              <Badge variant='secondary' key={version}>
+                {version}
+              </Badge>
+            ))}
+          </div>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle className='font-minecraft text-xl'>Compatibility</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className='text-sm'>No compatibility information available.</p>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle className='font-minecraft text-xl'>Authors</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className='flex flex-wrap gap-2'>
+            {authors.map((author) => (
+              <Badge variant='secondary' key={author}>
+                {author}
+              </Badge>
+            ))}
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
