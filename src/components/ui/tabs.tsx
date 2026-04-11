7a11cb503d441c407b9bfb3806e8045e283e3442
Yehor <omelianenkoyehor@proton.me>
2026-04-11 11:41:10 +0200

    add track

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -31,7 +31,7 @@ function TabsTrigger({ className, ...props }: React.ComponentProps<typeof TabsPr
     <TabsPrimitive.Trigger
       data-slot='tabs-trigger'
       className={cn(
-        "data-[state=active]:bg-accent data-[state=active]:text-white dark:data-[state=active]:text-foreground focus-visible:border-ring  focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border inline-flex flex-1 items-center justify-center gap-1.5 rounded-sm border border-transparent px-2 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        "data-[state=active]:bg-accent hover:cursor-pointer data-[state=active]:text-white dark:data-[state=active]:text-foreground focus-visible:border-ring  focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border inline-flex flex-1 items-center justify-center gap-1.5 rounded-sm border border-transparent px-2 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
         className
       )}
       {...props}
